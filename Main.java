378360c4325b0df51cd59a353ee2a8fdac219845
l22070066 <151755572+l22070066@users.noreply.github.com>
2024-04-13 17:53:03 -0600

    Updated Main.java

--- Main.java
+++ Main.java
@@ -9,7 +9,8 @@ public class Main {
         do{
             System.out.println("1. Precision o exactitud");
             System.out.println("2. Punto Fijo");
-            System.out.println("3. Salir");
+            System.out.println("3. Metodo de Jacobi");
+            System.out.println("4. Salir");
             a = UA.nextInt();
             switch (a){
                 case 1:
@@ -19,11 +20,13 @@ public class Main {
                     PuntoFijo.PuntoFijo();
                     break;
                 case 3:
+                    Jacobi.Jacobi();
+                case 4:
                     System.out.println("Gracias por usar nuestro programa");
                     break;
                 default:
                     System.out.println("Ingreso una opcion incorrecta");
             }
-        } while (a != 3);
+        } while (a != 4);
     }
 }
